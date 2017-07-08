9e52527a659cfded86db9c583ebbffdf18bb4b88
michaelcabus <cabuscreates@gmail.com>
2017-07-08 17:07:58 -0400

    initial site

---
+++ js/animation.js
@@ -0,0 +1,44 @@
+/*background:
+ linear-gradient(
+     rgba(60, 95, 112, 0.7),
+     rgba(60, 95, 112, 0.7)
+   ),
+   background:
+   linear-gradient(
+        rgba(60, 95, 112, 0.7),
+        rgba(60, 95, 112, 0.7)
+      )
+
+      linear-gradient(
+            rgba(60, 95, 112, 0.7),
+            rgba(60, 95, 112, 0.7)
+          ),
+          url(../images/carousel1.jpg) !important
+   */
+
+   $(document).ready(function() {
+  $(".events").fadeIn();
+     // place this within dom ready function
+     function showpanel() {
+
+       $('.events').css({background: "linear-gradient(rgba(60, 95, 112, 0.7),rgba(60, 95, 112, 0.7)), url(images/carousel1.jpg)"});
+    }
+
+    function showtext() {
+
+      $('.event_number').fadeIn();
+   }
+
+   function showevent() {
+
+     $('.event_name').fadeIn();
+  }
+
+    // use setTimeout() to execute
+    setTimeout(showpanel, 1000);
+
+    setTimeout(showtext, 4000);
+
+    setTimeout(showevent, 6000);
+
+   });
