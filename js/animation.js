16011d39d99b0a8c2ebfa8daf9303e86cae41f93
michaelcabus <cabuscreates@gmail.com>
2017-07-09 23:10:17 -0400

    fixing code

--- js/animation.js
+++ js/animation.js
@@ -1,12 +1,11 @@
 
 
    $(document).ready(function() {
-     window.alert("in function")
-  $(".events").fadeIn();
+
      // place this within dom ready function
      function showpanel() {
 
-       $('.events').css({background: "linear-gradient(rgba(60, 95, 112, 0.7),rgba(60, 95, 112, 0.7)), url(images/carousel1.jpg)"});
+        $("#events").fadeIn();
     }
 
     function showtext() {
@@ -17,6 +16,8 @@
    function showevent() {
 
      $('.event_name').fadeIn();
+    
+
   }
 
     // use setTimeout() to execute
